7f00c8e4cb8a6dc785a78e1bd3dc1e37ce55b6e7
ElihuTM <ruben_3o@hotmail.com>
2026-08-12 11:56:56 -0600

    12-08-2026-version-final

--- bloque1_fundamentos/dia01_variables.py
+++ bloque1_fundamentos/dia01_variables.py
@@ -1,3 +1,4 @@
+# ejercicio 1
 
 nombre = "Noly"
 edad = 31
@@ -10,3 +11,50 @@ print(edad)
 print(estatura)
 print(ciudad)
 print(me_gusta_programar)
+
+
+# ejercicio 2
+
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(ciudad))
+print(type(me_gusta_programar))
+
+
+# ejercicio 3
+
+
+contador = 0
+print(contador)
+
+contador = 50
+print(contador)
+
+contador = "cincuenta"
+print(type(contador))
+
+
+# ejercico 4
+
+numero_magico = "120"
+print(type(numero_magico))
+
+reasignacion = int(numero_magico)
+print(type(reasignacion))
+
+
+suma = reasignacion + 5
+print(suma)
+
+
+# ejercicio 5
+
+dia_actual = 1
+dias_totales = 120
+
+dias_restantes = dias_totales - dia_actual
+print(dias_restantes)
+
+
+# int("hola")
