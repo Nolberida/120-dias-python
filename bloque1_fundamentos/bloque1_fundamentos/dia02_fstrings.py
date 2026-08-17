d62fe029c1aa6657a3729f7cf7c596561e0959b1
ElihuTM <ruben_3o@hotmail.com>
2026-08-17 17:39:05 -0600

    Dia 2: f-strings, formato de decimales

---
+++ bloque1_fundamentos/bloque1_fundamentos/dia02_fstrings.py
@@ -0,0 +1,37 @@
+
+
+nombre = "Noly"
+edad = 31
+estatura = 1.59
+ciudad = "mexico"
+me_gusta_programar = True
+promedio_ventas = 2236.571428
+
+# ejercicio 1
+
+print(f"Hola soy {nombre} tengo {edad} años, mido {estatura} mestros, vivo en {ciudad} y ¿me gusta programar? {me_gusta_programar}")
+
+
+# ejercicio 2
+
+print(f"en 10 años tendre {edad + 10} y mi estatura sera {estatura * 100} cm")
+
+
+# ejercicio 3
+
+print(f"promedio de ventas: {promedio_ventas:.2f}")
+print(f"promedio de ventas: {promedio_ventas: .1f}")
+print(f"promedio de ventas: {promedio_ventas: .0f}")
+
+
+# 2236.57
+# 2236.5
+# 2236
+
+
+# ejemplo 4
+
+anio = input("en que año naciste?")
+nombre2 = input("cual es tu nombre?")
+
+print(f"hola {nombre2} , tu edad es {2026-int(anio)} años")
--- errores.md
+++ errores.md
@@ -1,1 +1,5 @@
 # Libreta de errores del reto
+## Error 1 — ValueError
+- Qué hice: int("hola")
+- Qué dijo Python: ValueError: invalid literal for int() with base 10: 'hola'
+- Por qué: int() solo convierte textos que parecen números
